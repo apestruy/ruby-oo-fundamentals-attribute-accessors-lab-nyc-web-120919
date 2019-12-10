5334ab5f9bb02473f71f50bce9d4c88e8caea936
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-10 19:13:09 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,6 +1,14 @@
 class Cat 
   attr_accessor :name 
-  
+  def initialize(meow)
+    @meow = meow
+  end 
+  def meow=(meow)
+    @meow = meow
+  end 
+  def meow
+    puts "meow!"
+  end 
   
   
   
